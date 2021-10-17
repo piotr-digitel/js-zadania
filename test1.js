c49cc491e188bda446b3e97f0843ee11e43da395
piotr-digitel <digitel@o2.pl>
2021-10-17 22:24:20 +0200

    bez 6 i 9

--- test1.js
+++ test1.js
@@ -28,15 +28,15 @@ while( n > 0 ){
     summary *= n--;
     //console.log(summary);
 }
-console.log("7 silnia = "+summary);
+console.log("7 silnia = " + summary);
 
 // 3) sum of the odd items
 let valueArray = [1,6,23,8,4,98,3,7,3,98,4,98];
 let sumodd = 0;
 for (let i = 0; i < valueArray.length; i++) {
-    if(valueArray[i] % 2 == 0) sumodd = sumodd + valueArray[i];
+    if(valueArray[i] % 2 != 0) sumodd = sumodd + valueArray[i];
 }
-console.log("Suma liczb parzystych to: " + sumodd);
+console.log("Suma liczb nieparzystych to: " + sumodd);
 
 
 // 4) Choose highest and lowest values from the given array. [1,6,23,8,4,98,3,7,3,98,4,98]. One loop run.
@@ -70,7 +70,7 @@ console.log("Najdłuższy napis to: " + valueArray5[longestindex]);
 
 
 
-//7) Calculate average value from the given array for even numbers [1,6,23,8,4,98,3,7,3,98,4,98]
+// 7) Calculate average value from the given array for even numbers [1,6,23,8,4,98,3,7,3,98,4,98]
 let valueArray7 = [1,6,23,8,4,98,3,7,3,98,4,98];
 let sum7 = 0;
 let avg7 = 0;
@@ -79,3 +79,17 @@ for (let i = 0; i < valueArray7.length; i++) {
 }
 avg7 = sum7 / valueArray7.length;
 console.log("Średnia to: " + avg7);
+
+// 8) Calculate average value of items at even indexes. Zero is not considered to be even number. [1,6,23,8,4,98,3,7,3,98,4,98]
+let valueArray8 = [1,6,23,8,4,98,3,7,3,98,4,98];
+let sum8 = 0;
+let avg8 = 0;
+let amount = 0;
+for (let i = 1; i < valueArray8.length; i++) {
+    if(i % 2 == 0) {
+        sum8 += valueArray8[i];
+        amount++;   
+    }
+}
+avg8 = sum8 / amount;
+console.log("Średnia liczb parzystych indexów to: " + avg8);
