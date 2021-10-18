654bb6ef0318d9a676cd85ce93ba017b8043a0ba
piotr-digitel <digitel@o2.pl>
2021-10-18 10:25:09 +0200

    Gotowe i sprawdzone

--- test1.js
+++ test1.js
@@ -16,8 +16,9 @@ let years = [1974, 1900, 1985, 2000];
 let year;
 for (let i = 0; i < years.length; i++) {
    year = years[i];
-   if ((year % 100 === 0 || year % 4 === 0) && year % 400 !== 0) {
-        console.log("Lata przestępne: " + year);
+   //if ((year % 100 === 0 || year % 4 === 0) && year % 400 !== 0) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 == 0) {
+        console.log("1) Lata przestępne: " + year);
    }
  }
 
@@ -28,7 +29,7 @@ while( n > 0 ){
     summary *= n--;
     //console.log(summary);
 }
-console.log("7 silnia = " + summary);
+console.log("2) 7 silnia = " + summary);
 
 // 3) sum of the odd items
 let valueArray = [1,6,23,8,4,98,3,7,3,98,4,98];
@@ -36,7 +37,7 @@ let sumodd = 0;
 for (let i = 0; i < valueArray.length; i++) {
     if(valueArray[i] % 2 != 0) sumodd = sumodd + valueArray[i];
 }
-console.log("Suma liczb nieparzystych to: " + sumodd);
+console.log("3) Suma liczb nieparzystych to: " + sumodd);
 
 
 // 4) Choose highest and lowest values from the given array. [1,6,23,8,4,98,3,7,3,98,4,98]. One loop run.
@@ -47,8 +48,8 @@ for (let i = 1; i < valueArray4.length; i++) {
     if(valueArray4[i] > highval) highval = valueArray4[i];
     if(valueArray4[i] < lowval) lowval = valueArray4[i];
 }
-console.log("Największa liczba to: " + highval);
-console.log("Najmniejsza liczba to: " + lowval);
+console.log("4) Największa liczba to: " + highval);
+console.log("   Najmniejsza liczba to: " + lowval);
 
 // 5) Choose longest string from the array. [‘Karol’, ‘Adam’,’Rogowski’,’Politechnika’,’Super’,’Weekend’].
 let valueArray5 = ['Karol', 'Adam','Rogowski','Politechnika','Super','Weekend'];
@@ -60,7 +61,7 @@ for (let i = 0; i < valueArray5.length; i++) {
         maxlenght = valueArray5[i].length
     }
 }
-console.log("Najdłuższy napis to: " + valueArray5[longestindex]);
+console.log("5) Najdłuższy napis to: " + valueArray5[longestindex]);
 
 // 6) Choose all the indexes on the highest value from the given array. [1,6,23,8,4,98,3,7,3,98,4,98].
 let valueArray6 = [1,6,23,8,4,98,3,7,3,98,4,98];
@@ -70,6 +71,7 @@ for (let i = 1; i < valueArray6.length; i++) {
     if(valueArray6[i] > highval6) highval6 = valueArray6[i];
 }
 //wydruk indexów zawierających największą wartość
+console.log("6) Indexy największej wartości: ");
 for (let i = 1; i < valueArray6.length; i++) {
     if(valueArray6[i] == highval6) console.log(i);
 }
@@ -78,11 +80,15 @@ for (let i = 1; i < valueArray6.length; i++) {
 let valueArray7 = [1,6,23,8,4,98,3,7,3,98,4,98];
 let sum7 = 0;
 let avg7 = 0;
+let amount7 = 0;
 for (let i = 0; i < valueArray7.length; i++) {
-    sum7 += valueArray7[i];
+    if(valueArray7[i] % 2 == 0) {
+        sum7 += valueArray7[i];
+        amount7++; 
+    }
 }
-avg7 = sum7 / valueArray7.length;
-console.log("Średnia to: " + avg7);
+avg7 = sum7 / amount7;
+console.log("7) Średnia parzystych to: " + avg7);
 
 // 8) Calculate average value of items at even indexes. Zero is not considered to be even number. [1,6,23,8,4,98,3,7,3,98,4,98]
 let valueArray8 = [1,6,23,8,4,98,3,7,3,98,4,98];
@@ -96,13 +102,44 @@ for (let i = 1; i < valueArray8.length; i++) {
     }
 }
 avg8 = sum8 / amount;
-console.log("Średnia liczb parzystych indexów to: " + avg8);
+console.log("8) Średnia liczb parzystych indexów to: " + avg8);
 
 // 9) With a given start value of 0. Iterate the array and add even items and subtract odd ones. [1,6,23,8,4,98,3,7,3,98,4,98]
 let valueArray9 = [1,6,23,8,4,98,3,7,3,98,4,98];
 let sum9 = 0;
 for (let i = 0; i < valueArray9.length-1; i = i + 2) {
-        sum9 += valueArray9[i];
-        sum9 -= valueArray9[i+1];
+        sum9 -= valueArray9[i];
+        sum9 += valueArray9[i+1];     
 }
-console.log("Suma parzystych po odjęciu nieparzystych to: " + sum9);
+console.log("9) Suma parzystych po odjęciu nieparzystych to: " + sum9);
+
+
+//Answers:
+
+// 1) Lata przestępne: 2000
+// 2) 7 silnia = 5040
+// 3) Suma liczb nieparzystych to: 37
+// 4) Największa liczba to: 98
+//    Najmniejsza liczba to: 1
+// 5) Najdłuższy napis to: Politechnika
+// 6) Indexy największej wartości:
+//    5
+//    9
+//    11
+// 7) Średnia parzystych to: 45.142857142857146
+// 8) Średnia liczb parzystych indexów to: 7.4
+// 9) Suma parzystych po odjęciu nieparzystych to: 277
+
+
+
+
+
+
+
+
+
+
+
+
+
+
