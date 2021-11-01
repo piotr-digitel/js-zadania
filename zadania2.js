690271be929fd9a4c56905a4b9bc7ca7fd049743
piotr-digitel <digitel@o2.pl>
2021-11-01 18:33:02 +0100

    bez 9

--- zadania2.js
+++ zadania2.js
@@ -100,7 +100,7 @@ console.log("6) New array2 : " + NewArray2(array));
 
 //     7) Create a function the return one random element from given array. // use random function 
 function RndArray(arrayIn){
-    max = Math.floor(arrayIn.length-1);
+    let max = Math.floor(arrayIn.length-1);
     return arrayIn[Math.floor(Math.random() * (max + 1))];
 }
 
@@ -109,3 +109,101 @@ console.log("7) Random element of array : " + RndArray(array));
 
 
 //     8) Create a function that takes two parameters: array and number off attempts. Based on number of attempts choose a random number from table that many times and return lowest one.
+function RndArray2(arrayIn, attempt){
+    let tmpArray = [];
+    let max = arrayIn.length;
+    for (let i = 0; i < attempt; i++) { 
+        tmpArray[i] = arrayIn[Math.floor(Math.random() * (max-1))];
+    }
+    //console.log(tmpArray);
+    return Math.min(...tmpArray); 
+}
+
+array = [1,3,4,1,0,3];
+console.log("8) Lowest, random element of array : " + RndArray2(array, 10));
+
+
+//     9) Create a function that takes given array. Then takes a random element, removes it from the array and pushes it to result arrays. This takes place as long as there are elements in source array. 
+
+
+
+
+
+
+
+
+
+
+
+//     10) Create a function that on given array will perform operation of adding or subtracting elements. Operation is to be chosen at random. And return a result.[a,b,c,d] =>(((a+-b)+-c)+-d)
+function RndArray3(arrayIn){
+    let resultOfCounting = arrayIn[0];
+    for (let i = 1; i < arrayIn.length; i++) { 
+        if(Math.floor(Math.random() * (2))){        //random from 0 to 1 - integer
+            resultOfCounting+=arrayIn[i];
+        }else{
+            resultOfCounting-=arrayIn[i];
+        }
+    }
+    return resultOfCounting; 
+}
+
+array = [1,3,4,1,0,3];
+console.log("10) [a,b,c,d] =>(((a+-b)+-c)+-d) : " + RndArray3(array));
+
+
+
+//     11) Create a function that will return the current day name in Polish. 
+function DayOfWeekPL(){
+    switch (new Date().getDay()) {
+        case 0:
+          day = "Niedziela";
+          break;
+        case 1:
+          day = "Poniedziałek";
+          break;
+        case 2:
+           day = "Wtorek";
+          break;
+        case 3:
+          day = "Środa";
+          break;
+        case 4:
+          day = "Czwartek";
+          break;
+        case 5:
+          day = "Piątek";
+          break;
+        case 6:
+          day = "Sobota";
+      }
+    return day; 
+}
+
+console.log("11) Current day name in Polish : " + DayOfWeekPL());
+
+
+//     12) Create a function that tells us how many days till Friday 
+function hmdtF(){
+    let days = new Date().getDay();
+    if(days==6) return days;
+    days-=5;
+    return Math.abs(days); 
+}
+
+console.log("12) How many days till Friday : " + hmdtF());
+
+
+
+//     13) Create a function that take two numbers and return the object with 4 fields. Result on 4 basic arithmetic operations. 
+function ReturnObj(x,y){
+    let obj = {
+        sub: x-y,
+        add: x+y,
+        mul: x*y,
+        div: x/y
+      };
+    return obj; 
+}
+
+console.log("13) Result on 4 basic arithmetic operations : " + JSON.stringify(ReturnObj(3,4)));
